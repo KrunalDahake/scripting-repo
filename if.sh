fe160c51d34c00b3b2df06968236751287d75368
krunaldahake <krunaldahake246@gmail.com>
2023-06-28 12:41:31 +0530

    lt

---
+++ if.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "enter the number" NUM
+
+if [ $NUM -gt 100]
+then
+echo "number is grater than 100"
+elif [ $NUM -eq 100 ]
+then 
+echo "number is equal to 100"
+else
+echo "number is less than 100"
+
+fi
